82a1e9b5eb27c4015a23daee78716dda5b5c05fc
lontonggg <zadareyhan@gmail.com>
2024-05-06 02:38:36 +0700

    [RED] Create User model skeleton

---
+++ src/main/java/snackscription/authentication/model/User.java
@@ -0,0 +1,23 @@
+package snackscription.authentication.model;
+
+public class User  {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+
+    public void setEmail(String email) {
+
+    }
+
+    public void setRole(String role) {
+
+    }
+
+    public void setPassword(String password) {
+
+    }
+}
