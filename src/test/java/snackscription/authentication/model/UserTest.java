2666b01cb9ece3ff4c5c60d3f1213ba52d220d22
lontonggg <zadareyhan@gmail.com>
2024-05-06 02:37:02 +0700

    [RED] Create test for User model

---
+++ src/test/java/snackscription/authentication/model/UserTest.java
@@ -0,0 +1,63 @@
+package snackscription.authentication.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+
+        user.setName("John Doe");
+        user.setEmail("john@example.com");
+        user.setPassword("password");
+        user.setRole("USER");
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("John Doe", user.getName());
+    }
+
+    @Test
+    void testGetEmail() {
+        assertEquals("john@example.com", user.getEmail());
+    }
+
+    @Test
+    void testGetPassword() {
+        assertEquals("password", user.getPassword());
+    }
+
+    @Test
+    void testGetRole() {
+        assertEquals("USER", user.getRole());
+    }
+
+    @Test
+    void testInvalidEmailFormat() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setEmail("invalid");
+        });
+    }
+
+    @Test
+    void testInvalidUserType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setRole("INVALID");
+        });
+    }
+
+    @Test
+    void testPasswordLessThan8Characters() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            user.setPassword("123");
+        });
+    }
+}
