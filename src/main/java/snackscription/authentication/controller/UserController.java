8ce3c3b097c0ecb9879a0c271e77ca92c92686aa
lontonggg <zadareyhan@gmail.com>
2024-05-06 14:36:23 +0700

    [GREEN] Implement User Skeleton

--- src/main/java/snackscription/authentication/controller/UserController.java
+++ src/main/java/snackscription/authentication/controller/UserController.java
@@ -1,44 +1,49 @@
 package snackscription.authentication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import snackscription.authentication.dto.UserDTO;
+import snackscription.authentication.service.UserServiceImpl;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
     @PostMapping("/auth/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO request){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.register(request));
     }
 
     @PostMapping("/auth/login")
     public ResponseEntity<UserDTO> login(@RequestBody UserDTO request){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.login(request));
     }
 
     @PostMapping("/auth/refresh")
     public ResponseEntity<UserDTO> refresh(@RequestBody UserDTO request){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.refreshToken(request));
     }
 
     @GetMapping("/admin/get-all-users")
     public ResponseEntity<UserDTO> getAllUser(){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.getAllUser());
     }
 
     @GetMapping("/admin/get-user/{userId}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable String userId){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.getUserById(userId));
     }
 
     @PutMapping("/useradmin/update/{userId}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable String userId, @RequestBody UserDTO request){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.updateUser(userId, request));
     }
 
     @DeleteMapping("/useradmin/delete/{userId}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable String userId){
-        return null;
+        return ResponseEntity.ok(userServiceImpl.deleteUser(userId));
     }
 }
