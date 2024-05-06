b9aff0a89b297a18781a0324d2905822ca7198a9
lontonggg <zadareyhan@gmail.com>
2024-05-06 14:40:00 +0700

    [REFACTOR] Changed field injection into constructor injection and change implementation dependency into interface dependency

--- src/main/java/snackscription/authentication/controller/UserController.java
+++ src/main/java/snackscription/authentication/controller/UserController.java
@@ -4,46 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import snackscription.authentication.dto.UserDTO;
-import snackscription.authentication.service.UserServiceImpl;
+import snackscription.authentication.service.UserService;
 
 @RestController
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    private UserServiceImpl userServiceImpl;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/auth/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userServiceImpl.register(request));
+        return ResponseEntity.ok(userService.register(request));
     }
 
     @PostMapping("/auth/login")
     public ResponseEntity<UserDTO> login(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userServiceImpl.login(request));
+        return ResponseEntity.ok(userService.login(request));
     }
 
     @PostMapping("/auth/refresh")
     public ResponseEntity<UserDTO> refresh(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userServiceImpl.refreshToken(request));
+        return ResponseEntity.ok(userService.refreshToken(request));
     }
 
     @GetMapping("/admin/get-all-users")
     public ResponseEntity<UserDTO> getAllUser(){
-        return ResponseEntity.ok(userServiceImpl.getAllUser());
+        return ResponseEntity.ok(userService.getAllUser());
     }
 
     @GetMapping("/admin/get-user/{userId}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable String userId){
-        return ResponseEntity.ok(userServiceImpl.getUserById(userId));
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 
     @PutMapping("/useradmin/update/{userId}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable String userId, @RequestBody UserDTO request){
-        return ResponseEntity.ok(userServiceImpl.updateUser(userId, request));
+        return ResponseEntity.ok(userService.updateUser(userId, request));
     }
 
     @DeleteMapping("/useradmin/delete/{userId}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable String userId){
-        return ResponseEntity.ok(userServiceImpl.deleteUser(userId));
+        return ResponseEntity.ok(userService.deleteUser(userId));
     }
 }
