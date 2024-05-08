650998a3159467c8fa20d43d7bbb6556a0775e8e
lontonggg <zadareyhan@gmail.com>
2024-05-08 16:01:29 +0700

    [FIX] Add better error response

--- src/main/java/snackscription/authentication/controller/UserController.java
+++ src/main/java/snackscription/authentication/controller/UserController.java
@@ -18,36 +18,76 @@ public class UserController {
 
     @PostMapping("/auth/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userService.register(request));
+        var response = userService.register(request);
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        return ResponseEntity.status(201).body(response);
     }
 
     @PostMapping("/auth/login")
     public ResponseEntity<UserDTO> login(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userService.login(request));
+        var response = userService.login(request);
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        } else if(response.getStatusCode() == 404){
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/auth/refresh")
     public ResponseEntity<UserDTO> refresh(@RequestBody UserDTO request){
-        return ResponseEntity.ok(userService.refreshToken(request));
+        var response = userService.refreshToken(request);
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/admin/get-all-users")
     public ResponseEntity<UserDTO> getAllUser(){
-        return ResponseEntity.ok(userService.getAllUser());
+        var response = userService.getAllUser();
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        else if(response.getStatusCode() == 404){
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/admin/get-user/{userId}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable String userId){
-        return ResponseEntity.ok(userService.getUserById(userId));
+       var response = userService.getUserById(userId);
+
+       if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/useradmin/update/{userId}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable String userId, @RequestBody UserDTO request){
-        return ResponseEntity.ok(userService.updateUser(userId, request));
+        var response = userService.updateUser(userId, request);
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        else if(response.getStatusCode() == 404){
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/useradmin/delete/{userId}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable String userId){
-        return ResponseEntity.ok(userService.deleteUser(userId));
+        var response = userService.deleteUser(userId);
+        if(response.getStatusCode() == 500){
+            return ResponseEntity.internalServerError().body(response);
+        }
+        else if(response.getStatusCode() == 404){
+            return ResponseEntity.status(404).body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 }
